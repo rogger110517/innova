78f9402df0fa6bcf08c4781929ccf9ff78efaa74
Rogger Palomino <palominocomv@gmail.com>
2021-12-08 09:38:54 -0500

    Realizando maquetación para la entrevista

---
+++ js/animaciones.js
@@ -0,0 +1,5 @@
+const web = document.querySelector('.gratitude_button_content_item');
+web.addEventListener('click', clickweb);
+function clickweb() {
+    window.location.href = "/proyecto-entrevista/proyecto.html"
+}
