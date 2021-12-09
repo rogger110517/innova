d7604564460f7d44adfce598545082618a28d542
Rogger Palomino <palominocomv@gmail.com>
2021-12-09 07:04:10 -0500

    Realizando maquetación para la entrevista

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,5 +1,5 @@
-const web = document.querySelector('.gratitude_button_content_item');
-const gratitude = document.querySelectorAll('.register_input_button')
+// const web = document.querySelector('.gratitude_button_content_item');
+// const gratitude = document.querySelectorAll('.register_input_button')
 const imagen1 = document.querySelectorAll('img');
  const cargarImagen = (entradas, observador) => {
     entradas.forEach((entrada) => {
@@ -17,12 +17,16 @@ const observador = new IntersectionObserver(cargarImagen,{
 imagen1.forEach((item) => {
     observador.observe(item);
 })
-gratitude.addEventListener('click' , clickregister)
-web.addEventListener('click', clickweb);
-function clickweb() {
-    window.location.href = "/innova/proyecto.html"
-}
-web.addEventListener('click', clickregister);
-function clickweb() {
-    window.location.href = "/innova/proyecto-agradecimiento.html"
-}
+// if(gratitude) {
+//     gratitude.addEventListener('click',clickregister)
+// }
+// if(web) {
+//     web.addEventListener('click', clickweb);
+// }
+// function clickweb() {
+//     window.location.href = "/innova/proyecto.html"
+// }
+// web.addEventListener('click', clickregister);
+// function clickregister() {
+//     window.location.href = "/innova/proyecto-agradecimiento.html"
+// }
---
+++ js/validaciones.js
@@ -0,0 +1,85 @@
+const btnEnviar = document.querySelector('.register_input_button')
+const form = document.querySelector('.register_content')
+const email = document.querySelector('#email')
+const tel= document.querySelector('#tel')
+const empresa = document.querySelector('#empresa')
+const select = document.querySelector('#select')
+const checked = document.querySelector('#checked')
+const formularioEnviar = document.querySelector('#enviar-form');
+
+const nombre = document.querySelector('#name')
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+
+eventListener();
+
+function eventListener() {
+    //cuando la app arranca
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+    //campos de formulario
+    email.addEventListener('blur', validarFormulario);
+    tel.addEventListener('blur', validarFormulario);
+    empresa.addEventListener('blur', validarFormulario);
+    nombre.addEventListener('blur', validarFormulario);
+    select.addEventListener('blur', validarFormulario);
+    checked.addEventListener('blur', validarFormulario);
+
+    formularioEnviar.addEventListener('submit', enviarEmail);
+}
+
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('restingir');
+}
+
+function validarFormulario(e) {
+    
+    if(e.target.type === 'checkbox') {
+        if(e.target.checked == true) {
+            alert("Gracias por la confianza")
+        } else {
+            alert("Por favor acepte las condiciones")
+        }
+    }
+    if(e.target.value.length > 0) {
+        const  errorP = document.querySelector('p.send-error');
+        if(errorP) {
+            errorP.remove();
+        }
+        e.target.classList.remove('placeholder-active')
+    } else {
+        e.target.classList.add('placeholder-active')
+
+        mostrarError('Todos los campos son obligatorios');
+    }
+    if(e.target.type === 'email'){
+        if(er.test(e.target.value)){
+            const  errorP = document.querySelector('p.send-error');
+            if(errorP) {
+                errorP.remove();
+            }
+            e.target.classList.remove('placeholder-active')
+        } else {
+            e.target.classList.add('placeholder-active')
+            mostrarError('El email no es valido');
+        }
+    }
+    if(er.test(email.value) !== '' && tel.value !== '' && empresa.value !== '' && nombre.value !== '' && checked.checked == true && select.value !== "0"){
+        btnEnviar.disabled = false;
+        btnEnviar.classList.remove('restingir')
+    }
+}
+function mostrarError(mensaje) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('send-error');
+    const errores = document.querySelectorAll('.send-error');
+
+    if(errores.length === 0) {
+        form.appendChild(mensajeError); 
+    }
+}
+function enviarEmail(e) {
+    e.preventDefault();
+    window.location.href ='/proyecto-entrevista/proyecto-agradecimiento.html';
+}
